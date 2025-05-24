8b6b7f9f752b4513f0555efbbf965e42140468c2
KwenzyZZ <zaidanhasbiakbar345@gmail.com>
2025-05-25 01:01:02 +0700

    Menambahkan Portfolio(60%)

---
+++ scriptPortfolio.js
@@ -0,0 +1,19 @@
+AOS.init();
+
+const body = document.body;
+ body.style.transition = 'background-color 0.5s ease';
+  document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('mouseenter', () => {
+      if (link.classList.contains('home')) {
+        body.style.backgroundColor = '#f3f3ef';
+      } else if (link.classList.contains('portfolio')) {
+        body.style.backgroundColor = '#0ba13b';
+      } else if (link.classList.contains('contact')) {
+        body.style.backgroundColor = '#ff8743';
+      }
+    });
+
+    link.addEventListener('mouseleave', () => {
+      body.style.backgroundColor = '';
+    });
+  });
